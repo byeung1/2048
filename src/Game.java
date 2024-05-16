2f9f948fa4540df4632500dac95db6530c5109c8
Byeung1 <brandondyeung@gmail.com>
2024-05-16 10:45:43 -0400

    added while loop and switch statement

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,27 @@
+import java.util.*;
+
 public class Game {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         Board board = new Board();
         board.createNewBoard();
+
+        board.printBoard();
+
+        while (true) {
+
+            switch (sc.nextLine()) {
+                case "A": //for moving left
+                    for (int i = 0; i < 4; i++) {
+                        
+                    }
+                    break;
+            
+                default:
+                    break;
+            }
+
+        }
     }
 }
