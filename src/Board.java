dbd1b60242de6331e18e64ab2af04f5ce6246d3d
Jerry Yao <99371069+Jerry-0215@users.noreply.github.com>
2024-05-16 10:42:50 -0400

    Added getter and setter for Board

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,31 @@
 public class Board {
 
-    public static int [][] gameBoard;
+    public static int[][] gameBoard;
 
+    /** 
+     * Initializes new board with 0's
+     */
     public static void createNewBoard() {
-        gameBoard = new int[4][4];
+        gameBoard=new int[4][4];
+    }
+
+    public static void generateTwoTiles() {
+        
+    }
+
+    public int getValue (int row, int column) {
+        return gameBoard[row][column];
+    }
+
+    public void setValue (int row, int column, int value) {
+        gameBoard[row][column]=value;
+    }
+    
+    public static void printBoard()
+    {
+        for (int i=0;i<4;i++)
+        {
+            
+        }
     }
 }
