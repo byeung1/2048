52f426074c2ba11527116e313b5506719fab071b
Byeung1 <brandondyeung@gmail.com>
2024-05-18 11:18:11 -0400

    fixed the boolean array, stopped tiles from generating if no move is made

--- src/Game.java
+++ src/Game.java
@@ -17,25 +17,38 @@ public class Game {
 
         while (!board.isGameOver()) {
 
-            switch (sc.nextLine()) {
-                case "a": //for moving left
-                    for (int i = 0; i < 4; i++)
-                        left(i, board);
-                    break;
-                case "d": //for moving right
-                    for (int i = 0; i < 4; i++)
-                        right(i, board);
-                    break;
-                case "w": //for moving up
-                    for (int i = 0; i < 4; i++)
-                        up(i, board);
-                    break;
-                case "s": //for moving down
-                    for (int i = 0; i < 4; i++)
-                        down(i, board);
-                    break;
-                default:
-                    break;
+            outer:
+            while (true) {
+
+                switch (sc.nextLine()) {
+                    case "a": //for moving left
+                        if (left(board)) {
+                            board.printBoard();
+                            break outer;
+                        }
+                        break;
+                    case "d": //for moving right
+                        if (right(board)) {
+                            board.printBoard();
+                            break outer;
+                        }
+                        break;
+                    case "w": //for moving up
+                        if (up(board)) {
+                            board.printBoard();
+                            break outer;
+                        }
+                        break;
+                    case "s": //for moving down
+                        if (down(board)) {
+                            board.printBoard();
+                            break outer;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
             }
 
             board.generateNewTile();
@@ -46,115 +59,151 @@ public class Game {
         }
     }
 
-    public static void left(int row, Board board) {
-        boolean[] merged = new boolean[4];
-        for (int j = 1; j < 4; j++) {
-            while (true) {
+    public static boolean left(Board board) {
 
-                if(board.getValue(row, j) == 0 || j - 1 == -1)
-                    break;
+        boolean moved = false;
+        for (int row = 0; row < 4; row++) {
+            
+            boolean[] merged = new boolean[4];
+            for (int j = 1; j < 4; j++) {
+                while (true) {
 
-                if (board.getValue(row, j - 1) == 0) {
-                    board.setValue(row, j - 1, board.getValue(row, j));
-                    board.setValue(row, j, 0);
+                    if(board.getValue(row, j) == 0 || j - 1 == -1)
+                        break;
+
+                    if (board.getValue(row, j - 1) == 0) {
+                        board.setValue(row, j - 1, board.getValue(row, j));
+                        board.setValue(row, j, 0);
+                        moved = true;
+                    }
+                    else if (board.getValue(row, j - 1) == board.getValue(row, j) && (!merged[j - 1] && !merged[j])) {
+                        merged[j - 1] = true;
+                        board.setValue(row, j - 1, 2*board.getValue(row, j - 1));
+                        score += board.getValue(row, j - 1);
+                        board.setValue(row, j, 0);
+                        moved = true;
+                    }
+                    else {
+                        break;
+                    }
+
+                    j--;
+
                 }
-                else if (board.getValue(row, j - 1) == board.getValue(row, j) && !merged[j - 1] && !merged[j]) {
-                    merged[j - 1] = true;
-                    board.setValue(row, j - 1, 2*board.getValue(row, j - 1));
-                    score += board.getValue(row, j - 1);
-                    board.setValue(row, j, 0);
-                }
-                else {
-                    break;
-                }
-
-                j--;
-
             }
         }
+
+        return moved;
     }
 
-    public static void right(int row, Board board) {
-        boolean[] merged = new boolean[4];
-        for (int j = 3; j > -1; j--) {
-            while (true) {
+    public static boolean right(Board board) {
 
-                if(board.getValue(row, j) == 0 || j + 1 == 4)
-                    break;
+        boolean moved = false;
+        for (int row = 0; row < 4; row++) {
 
-                if (board.getValue(row, j + 1) == 0) {
-                    board.setValue(row, j + 1, board.getValue(row, j));
-                    board.setValue(row, j, 0);
+            boolean[] merged = new boolean[4];
+            for (int j = 2; j > -1; j--) {
+                while (true) {
+
+                    if(board.getValue(row, j) == 0 || j + 1 == 4)
+                        break;
+
+                    if (board.getValue(row, j + 1) == 0) {
+                        board.setValue(row, j + 1, board.getValue(row, j));
+                        board.setValue(row, j, 0);
+                        moved = true;
+                    }
+                    else if (board.getValue(row, j + 1) == board.getValue(row, j) && (!merged[j + 1] && !merged[j])) {
+                        merged[j + 1] = true;
+                        board.setValue(row, j + 1, 2*board.getValue(row, j + 1));
+                        score += board.getValue(row, j + 1);
+                        board.setValue(row, j, 0);
+                        moved = true;
+                    }
+                    else {
+                        break;
+                    }
+
+                    j++;
+
                 }
-                else if (board.getValue(row, j + 1) == board.getValue(row, j) && !merged[j + 1] && !merged[j]) {
-                    merged[j + 1] = true;
-                    board.setValue(row, j + 1, 2*board.getValue(row, j + 1));
-                    score += board.getValue(row, j + 1);
-                    board.setValue(row, j, 0);
-                }
-                else {
-                    break;
-                }
-
-                j++;
-
             }
         }
+
+        return moved;
     }
 
-    public static void up(int column, Board board) {
-        boolean[] merged = new boolean[4];
-        for (int j = 1; j < 4; j++) {
-            while (true) {
+    public static boolean up(Board board) {
 
-                if(board.getValue(j, column) == 0 || j - 1 == -1)
-                    break;
+        boolean moved = false;
+        for (int column = 0; column < 4; column++) {
 
-                if (board.getValue(j - 1, column) == 0) {
-                    board.setValue(j - 1, column, board.getValue(j, column));
-                    board.setValue(j, column, 0);
+            boolean[] merged = new boolean[4];
+            for (int j = 1; j < 4; j++) {
+                while (true) {
+
+                    if(board.getValue(j, column) == 0 || j - 1 == -1)
+                        break;
+
+                    if (board.getValue(j - 1, column) == 0) {
+                        board.setValue(j - 1, column, board.getValue(j, column));
+                        board.setValue(j, column, 0);
+                        moved = true;
+                    }
+                    else if (board.getValue(j - 1, column) == board.getValue(j, column) && (!merged[j - 1] && !merged[j])) {
+                        merged[j - 1] = true;
+                        board.setValue(j - 1, column, 2*board.getValue(j - 1, column));
+                        score += board.getValue(j - 1, column);
+                        board.setValue(j, column, 0);
+                        moved = true;
+                    }
+                    else {
+                        break;
+                    }
+
+                    j--;
+
                 }
-                else if (board.getValue(j - 1, column) == board.getValue(j, column) && !merged[j + 1] && !merged[j]) {
-                    merged[j + 1] = true;
-                    board.setValue(j - 1, column, 2*board.getValue(j - 1, column));
-                    score += board.getValue(j - 1, column);
-                    board.setValue(j, column, 0);
-                }
-                else {
-                    break;
-                }
-
-                j--;
-
             }
         }
+
+        return moved;
     }
 
-    public static void down(int column, Board board) {
-        boolean[] merged = new boolean[4];
-        for (int j = 3; j > -1; j--) {
-            while (true) {
+    public static boolean down(Board board) {
 
-                if(board.getValue(j, column) == 0 || j + 1 == 4)
-                    break;
+        boolean moved = false;
+        for (int column = 0; column < 4; column++) {
 
-                if (board.getValue(j + 1, column) == 0) {
-                    board.setValue(j + 1, column, board.getValue(j, column));
-                    board.setValue(j, column, 0);
+            boolean[] merged = new boolean[4];
+            for (int j = 2; j > -1; j--) {
+                while (true) {
+
+                    if(board.getValue(j, column) == 0 || j + 1 == 4)
+                        break;
+
+                    if (board.getValue(j + 1, column) == 0) {
+                        board.setValue(j + 1, column, board.getValue(j, column));
+                        board.setValue(j, column, 0);
+                        moved = true;
+                    }
+                    else if (board.getValue(j + 1, column) == board.getValue(j, column) && (!merged[j + 1] && !merged[j])) {
+                        merged[j + 1] = true;
+                        board.setValue(j + 1, column, 2*board.getValue(j + 1, column));
+                        score += board.getValue(j + 1, column);
+                        board.setValue(j, column, 0);
+                        moved = true;
+                    }
+                    else {
+                        break;
+                    }
+
+                    j++;
+
                 }
-                else if (board.getValue(j + 1, column) == board.getValue(j, column) && !merged[j - 1] && !merged[j]) {
-                    merged[j - 1] = true;
-                    board.setValue(j + 1, column, 2*board.getValue(j + 1, column));
-                    score += board.getValue(j + 1, column);
-                    board.setValue(j, column, 0);
-                }
-                else {
-                    break;
-                }
-
-                j++;
-
             }
         }
+
+        return moved;
     }
 }
